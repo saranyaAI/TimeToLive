9345fb97a0a964744887f0bc4845a88df3dcdf13
saranyaAI <madavanerisaranya@gmail.com>
2026-03-12 10:16:32 +0530

    saranya Added usecase3

--- TimeLive/src/TimeToLive.java
+++ TimeLive/src/TimeToLive.java
@@ -13,20 +13,22 @@ class DNSEntry {
         return System.currentTimeMillis() > expiryTime;
     }
 }
+
 public class TimeToLive {
 
-    static HashMap<String, DNSEntry> cache = new HashMap<>();
+    static HashMap<String, DNSEntry> dnsCache = new HashMap<>();
     static int hits = 0, misses = 0;
 
+    // Simulated upstream DNS lookup
     static String queryUpstream(String domain) {
-        return "172.217.14.206"; // simulated IP
+        return "10.0.0.25";
     }
 
     static void resolve(String domain) {
 
-        if (cache.containsKey(domain)) {
+        if (dnsCache.containsKey(domain)) {
 
-            DNSEntry entry = cache.get(domain);
+            DNSEntry entry = dnsCache.get(domain);
 
             if (!entry.isExpired()) {
                 hits++;
@@ -34,33 +36,35 @@ public class TimeToLive {
                 return;
             } else {
                 System.out.println("resolve(\"" + domain + "\") → Cache EXPIRED");
-                cache.remove(domain);
+                dnsCache.remove(domain);
             }
         }
 
         misses++;
         String ip = queryUpstream(domain);
 
-        cache.put(domain, new DNSEntry(ip, 300));
+        dnsCache.put(domain, new DNSEntry(ip, 180));
 
-        System.out.println("resolve(\"" + domain + "\") → Cache MISS → Query upstream → " + ip + " (TTL: 300s)");
+        System.out.println("resolve(\"" + domain + "\") → Cache MISS → Query upstream → "
+                + ip + " (TTL: 180s)");
     }
 
-    static void getStats() {
-        int total = hits + misses;
-        double rate = (total == 0) ? 0 : (hits * 100.0 / total);
+    static void getCacheStats() {
 
-        System.out.println("Hit Rate: " + rate + "%");
+        int total = hits + misses;
+        double hitRate = (total == 0) ? 0 : (hits * 100.0 / total);
+
+        System.out.println("Cache Hit Rate: " + hitRate + "%");
     }
 
     public static void main(String[] args) {
 
-        resolve("google.com");
-        resolve("google.com");
+        resolve("intranet.company.com");
+        resolve("intranet.company.com");
 
-        resolve("openai.com");
-        resolve("google.com");
+        resolve("mail.company.com");
+        resolve("intranet.company.com");
 
-        getStats();
+        getCacheStats();
     }
 }
